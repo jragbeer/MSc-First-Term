865c714fd2e4b53ac5c7cd071269d8d2c4ecf4ab
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-12-03 17:53:34 -0500

    Updated with answer

--- lab8.py
+++ lab8.py
@@ -1,81 +1,50 @@
-import pandas as pd
 import numpy as np
-import random
-from matplotlib import style
-import matplotlib.pyplot as plt
-style.use('ggplot')
 
-pt1 = [3, 4]
-pt2 = [12, 14]
+np.random.seed(14235)
 
-np.random.seed(13)
+var = 10
 
-array1x = [np.random.randint(1,10)*0.1*pt1[0] for x in range(20)]
-array1y = [np.random.randint(1,10)*0.1*pt1[1] for x in range(20)]
-array2x = [np.random.randint(1,10)*0.1*pt2[0] for x in range(20)]
-array2y = [np.random.randint(1,10)*0.1*pt2[1] for x in range(20)]
+a = [(np.random.randint(-var,var)*0.1+1,np.random.randint(-var,var)*0.1+1,0) for x in range(20)]
+b = [(np.random.randint(-var,var)*0.1+3,np.random.randint(-var,var)*0.1+3,1) for x in range(20)]
 
-# df = pd.DataFrame(data = {'X':array1x+array2x, 'Y':array1y+array2y, 'class0':[1 for x in range(20)]+ [0 for x in range(20)], 'class1':[0 for x in range(20)]+ [1 for x in range(20)]})
-#
-# df0 = df[df.class0 == 1].copy()
-# df1 = df[df.class1 == 1].copy()
-#
-# print(df0.mean())
-# print(df1.mean())
-#
-# print(df)
-# print(df.cov())
-# print(df.mean())
-#
-# plt.scatter(df.X, df.Y)
+dataset = a+b
+np.random.shuffle(dataset)
 
-X = [[array1x[x], array1y[x], 0] for x in range(len(array1x))]
-ARRAY1 = X.copy()
+train = dataset[:32]
+test = dataset[32:]
 
-X2 = [[array2x[x], array2y[x], 1] for x in range(len(array1x))]
-ARRAY2 = X2.copy()
+p_c0 = len([x for x in dataset if x[2] == 0])/len(dataset)
+p_c1 = len([x for x in dataset if x[2] == 1])/len(dataset)
 
-for i in range(len(X2)):
-    X.append(X2[i])
+mean_c0 = [np.mean([x[0] for x in a]), np.mean([x[1] for x in a])]
+mean_c1 = [np.mean([x[0] for x in b]), np.mean([x[1] for x in b])]
 
-random.shuffle(X)
+meanvec = np.array([mean_c0,
+           mean_c1]).reshape((2,2))
 
-X_train = X[:33]
-X_test = X[33:]
+def make_cov(m):
+    u = [x[0] for x in m]
+    i = [x[1] for x in m]
+    xx = np.column_stack([u,i])
+    xx -= xx.mean(axis = 0)
+    cc = np.dot(xx.T, xx.conj())/len(u)
+    return cc
 
+class0covmatrix = make_cov(a)
+class1covmatrix = make_cov(b)
 
-X_train0mean = np.array(X_train)[:,0].mean()
-X_train1mean = np.array(X_train)[:,1].mean()
+def func(y, mvec,logpc, covmat, d):
+    y = np.array([y[0],y[1]])
+    first = d*np.log(2*np.pi)/2
+    second = 0.5*np.log(np.linalg.det(covmat))
+    third = 0.5*np.dot(np.array(y-mvec).T,np.linalg.inv(covmat))
+    third2 = np.dot(third,np.array(y-mvec))
+    return np.log(logpc)-first-second-third2
 
-
-def disfunc(x, d, cov_mat, p_C):
-    x0 = x[0]
-    x1 =x[1]
-    first = d/2*np.log(2*np.pi)
-    second = 0.5*np.log(np.linalg.det(cov_mat))
-    third = 0.5*(np.dot( np.array([x0-X_train0mean,x1-X_train1mean]).T, np.dot(np.linalg.inv(cov_mat), [x0-X_train0mean,x1-X_train1mean])))
-    return np.log(p_C) - first -second - third
-
-for x in ARRAY1:
-    print(x)
-
-p_C1 = sum(np.array(X_train)[:,2])/len(X_train)
-p_C2 = 1-p_C1
-
-print(p_C1)
-print(p_C2)
-class1 = []
-class0 = []
-for x in X:
-    if disfunc(x, 2, np.cov(X_train), p_C1)>disfunc(x, 2, np.cov(X_train), p_C2):
-        class1.append(x)
+for x in dataset:
+    class1 = func(x, mean_c0, p_c0, class0covmatrix, 2)
+    class2 = func(x, mean_c1, p_c1, class1covmatrix, 2)
+    if class1 > class2:
+        print(x, '0')
     else:
-        class0.append(x)
-
-print(class1)
-print(class0)
-
-
-plt.scatter(array1x, array1y, color = 'red')
-plt.scatter(array2x, array2y, color = 'blue')
-plt.show()
+        print(x, '1')
