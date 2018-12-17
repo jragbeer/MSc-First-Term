cc8e1660012137b18efb31de49a3fb421e61635b
JRagbeer <30732548+jragbeer@users.noreply.github.com>
2018-12-16 23:38:38 -0500

    Profiling Dask implemenation of PE62

---
+++ ParallelProjectEuler62.py
@@ -0,0 +1,73 @@
+import itertools
+import cProfile
+import numpy as np
+import io
+import pstats
+import datetime
+import dask
+from dask.distributed import Client, progress
+import dask.array as da
+
+
+
+if __name__ == '__main__':
+
+
+    def profile(fnc):
+        """A decorator that uses cProfile to profile a function"""
+
+        def inner(*args, **kwargs):
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = fnc(*args, **kwargs)
+            pr.disable()
+            s = io.StringIO()
+            sortby = 'cumulative'
+            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            ps.print_stats()
+            print(s.getvalue())
+            return retval
+
+        return inner
+
+
+    client = Client(threads_per_worker=1, n_workers=4)
+    client.restart()
+    @profile
+    def main():
+        timee = datetime.datetime.now()
+        tt = 1
+        # p = da.from_array([str(x ** 3) for x in range(tt, 501)], chunks = (500,))
+        p = [str(x ** 3) for x in range(tt, 1001)]
+        print(timee)
+
+        def w(xx):
+            return itertools.permutations(xx)
+
+        def tring(xx):
+            return ''.join(xx)
+
+        @dask.delayed
+        def dostuff(xx):
+            a = np.unique([tring(y) for y in w(xx)])
+            return a
+
+        arrays = [da.from_delayed(dostuff(v), shape = (1,), dtype=str) for v in p]
+
+        b = dask.compute(*arrays)
+        print(datetime.datetime.now()-timee)
+
+        @dask.delayed
+        def rtt(a, rr):
+            if len([i for i in rr if i in p]) == 3:
+                return a + tt
+
+
+        op = []
+        for q, x in enumerate(b):
+            op.append(rtt(q,x))
+
+        print(set(dask.compute(*op)))
+
+        print(datetime.datetime.now() - timee)
+    main()
